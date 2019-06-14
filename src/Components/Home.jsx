6d8f06cadf95b99e26fd9c4be41a061acb50700a
Ari Mendelow <ari@mendelow.me>
2019-06-14 18:15:11 -0400

    Basic login works

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
-	render() { 
-		return (
-			<div>
-				This is my home page!
-			</div>
-		);
-	}
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inputVal: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e, key) {
+    const { value } = e.target;
+    this.setState({ [key]: value });
+  }
+
+  render() {
+    const { logIn } = this.props;
+    const { inputVal } = this.state;
+    return (
+      <div>
+        <form onSubmit={e => logIn(e, inputVal)}>
+          <input
+            type='text'
+            name='inputVal'
+            placeholder='Username (email)'
+            value={inputVal}
+            onChange={e => this.handleChange(e, "inputVal")}
+          />
+          <button type='submit'>Login</button>
+        </form>
+      </div>
+    );
+  }
 }
- 
+
 export default Home;
