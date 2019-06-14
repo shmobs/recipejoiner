2440ffab42c489fe3ebd9526dc0e27d9cf03cfdd
Ari Mendelow <ari@mendelow.me>
2019-06-14 11:34:33 -0400

    Add App.jsx

---
+++ src/Components/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { broswerHistory } from 'react-router';
+
+class App extends Component {
+	render() {
+		return (
+			<Router>
+				// Routes will go here
+			</Router>
+			);
+	}
+}
