f67535f7857ff8c80120b8dbf3d480ceb50f7a51
Ari Mendelow <ari@mendelow.me>
2019-06-14 11:40:31 -0400

    Add 'Home' component

---
+++ src/Components/Home.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+	render() { 
+		return (
+			<div>
+				This is my home page!
+			</div>
+		);
+	}
+}
+ 
+export default Home;
