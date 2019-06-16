98d25bbe4d7a55fc97863531d593464c42dbc1e9
Joey Pinhas <jpinhas97@gmail.com>
2019-06-16 02:02:59 -0400

    Clean up some more

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import NavBar from './NavBar';
-import Home from './Home';
-import Dashboard from './Dashboard';
-import Recipe from './Recipe';
-import CreateRecipe from './Recipes/CreateRecipe';
+
+import NavBar from './NavBar.jsx';
+import Home from './Home.jsx';
+import Dashboard from './Dashboard.jsx';
+import Recipe from './Recipe.jsx';
+import CreateRecipe from './Recipes/CreateRecipe.jsx';
 
 
 class App extends Component {
@@ -29,60 +30,58 @@ class App extends Component {
   render() {
     const { isLoggedIn, activeUser } = this.state;
     return (
-      <React.Fragment>
-        <Router>
-          <NavBar />
-          <Route
-            name='home'
-            exact
-            path='/'
-            render={props => (
-              <Home
-                isLoggedIn={isLoggedIn}
-                activeUser={activeUser}
-                logIn={this.logIn}
-                {...props}
-              />
-            )}
-          />
-          <Route
-            name='dashboard'
-            exact
-            path='/dashboard'
-            render={props => (
-              <Dashboard
-                isLoggedIn={isLoggedIn}
-                activeUser={activeUser}
-                {...props}
-              />
-            )}
-          />
-          <Route
-            name='recipe'
-            exact
-            path='/recipes/:id'
-            render={props => (
-              <Recipe
-                isLoggedIn={isLoggedIn}
-                activeUser={activeUser}
-                {...props}
-              />
-            )}
-          />
-          <Route
-            name='create'
-            exact
-            path='/create'
-            render={props => (
-              <CreateRecipe
-                isLoggedIn={isLoggedIn}
-                activeUser={activeUser}
-                {...props}
-              />
-            )}
-          />
-        </Router>
-      </React.Fragment>
+      <Router>
+        <NavBar />
+        <Route
+          name='home'
+          exact
+          path='/'
+          render={props => (
+            <Home
+              isLoggedIn={isLoggedIn}
+              activeUser={activeUser}
+              logIn={this.logIn}
+              {...props}
+            />
+          )}
+        />
+        <Route
+          name='dashboard'
+          exact
+          path='/dashboard'
+          render={props => (
+            <Dashboard
+              isLoggedIn={isLoggedIn}
+              activeUser={activeUser}
+              {...props}
+            />
+          )}
+        />
+        <Route
+          name='recipe'
+          exact
+          path='/recipes/:id'
+          render={props => (
+            <Recipe
+              isLoggedIn={isLoggedIn}
+              activeUser={activeUser}
+              {...props}
+            />
+          )}
+        />
+        <Route
+          name='create'
+          exact
+          path='/create'
+          render={props => (
+            <CreateRecipe
+              isLoggedIn={isLoggedIn}
+              activeUser={activeUser}
+              {...props}
+            />
+          )}
+        />
+      </Router>
     );
   }
 }
