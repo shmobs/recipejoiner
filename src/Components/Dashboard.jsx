7c981127766a723630c2c2998d4d00179c2a88c9
Perry Raskin <perryraskin@gmail.com>
2019-06-15 22:08:48 -0400

    Categories page displays list of categories fetched from API

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 
 class Dashboard extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api/categories')
+      .then(r => r.json())
+      .then((data) => {
+        this.setState({ categories: data.categories });
+      })
+      .catch(err => console.log(err));
   }
 
   render() {
+    const { categories } = this.state;
+    const mappedCategories = categories.map(category => <li key={category}>{category}</li>);
+
     return (
       <div>
         <h1>Categories</h1>
-        
+        <ul>{mappedCategories}</ul>
       </div>
     );
   }
