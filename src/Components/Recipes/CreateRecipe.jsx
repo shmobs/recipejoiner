b3bc7e268972de5d9d842ccdbe4bee4ecb30b108
Ari Mendelow <ari@mendelow.me>
2019-06-16 01:48:28 -0400

    Update CreateRecipe to include the active userID

--- src/Components/Recipes/CreateRecipe.jsx
+++ src/Components/Recipes/CreateRecipe.jsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import Form from './Form';
 
-function submitNewRecipe(e, recipe) {
-  e.preventDefault();
-  console.log('in submit', recipe);
-  const { title, description } = recipe || {};
+function CreateRecipe(props) {
 
-  if (!title || !description) {
-    return false; // Returning false from a submit function prevents the form from being sumitted
+  const { activeUser } = props;
+
+  function submitNewRecipe(e, recipe) {
+    e.preventDefault();
+    console.log('in submit', recipe);
+    const { title, description } = recipe || {};
+
+    if (!title || !description) {
+      return false; // Returning false from a submit function prevents the form from being sumitted
+    }
+
+    fetch(`/api/recipes?user=${activeUser}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(recipe),
+    })
+      .then(r => r.json())
+      .then(console.log)
+      .catch(err => console.error(err));
+    return true;
   }
 
-  fetch('/api/recipes', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(recipe),
-  })
-    .then(r => r.json())
-    .then(console.log)
-    .catch(err => console.error(err));
-  return true;
-}
-
-function CreateRecipe() {
   return (
     <Form
       handleSubmit={submitNewRecipe}
