31d66e9641efe53b61c9450d56ab07793f28dd94
Perry Raskin <perryraskin@gmail.com>
2019-06-16 00:20:23 -0400

    Add Recipe component

---
+++ src/Components/Recipe.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class Recipe extends Component {
+  constructor(props) {
+    super(props);
+
+    const { match } = props;
+    const { params } = match || {};
+    const { id } = params || {};
+
+    this.state = {
+      id,
+      data: {},
+    };
+  }
+
+  componentDidMount() {
+    const { match } = this.props;
+    const { params } = match || {};
+    const { id } = params || {};
+
+    fetch(`/api/recipes/${id}`)
+      .then(r => r.json())
+      .then((data) => {
+        this.setState({ data });
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    const { data } = this.state;
+    return (
+      <div>
+        <h1>{data.title}</h1>
+        <p>{data.description}</p>
+      </div>
+    );
+  }
+}
+
+export default Recipe;
