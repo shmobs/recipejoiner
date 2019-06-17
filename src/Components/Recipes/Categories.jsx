e5b3f383d5d7d81a9fe7d85d64054bd8707325e6
Ari Mendelow <ari@mendelow.me>
2019-06-17 11:32:09 -0500

    Categories are now styled in the form

--- src/Components/Recipes/Categories.jsx
+++ src/Components/Recipes/Categories.jsx
@@ -3,10 +3,11 @@ import Category from './Category';
 
 function Categories(props) {
   const { deleteCategory, categories } = props;
+  const cats = categories.map((c, i) => (<Category key={c} category={c} handleDelete={() => deleteCategory(i)} />));
   return (
-    categories.map((c, i) => (
-      <Category key={c} category={c} handleDelete={() => deleteCategory(i)} />
-    ))
+    <div className='flex flex-wrap mb-2'>
+      { cats }
+    </div>
   );
 }
 
--- src/Components/Recipes/Category.jsx
+++ src/Components/Recipes/Category.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 function Category(props) {
   const { category, handleDelete } = props;
   return (
-    <span>
+    <span
+      className='bg-green-500 text-white rounded py-2 px-2 mr-2 mb-2'
+    >
       {category}
       <button
         type='button'
         onClick={handleDelete}
       >
-        &nbsp;<b>x</b>&nbsp;&nbsp;
+        &nbsp;<b>x</b>
       </button>
     </span>
   );
