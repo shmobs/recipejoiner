a2d0bf33138658f1906871ef0c9931e0ffc44539
Perry Raskin <perryraskin@gmail.com>
2019-07-11 14:46:14 -0400

    Missed a period. Very important.

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -84,7 +84,7 @@ class Home extends Component {
             </div>
           </form>
           <p className='text-center text-gray-500 text-xs'>
-            &copy;2019 RecipeJoiner All rights reserved.
+            &copy;2019 RecipeJoiner. All rights reserved.
           </p>
           {/*</div>*/}
       </div>
