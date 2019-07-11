24e8f778b0fd395543d5513353f21bb10ce6f4d5
Perry Raskin <perryraskin@gmail.com>
2019-07-11 14:19:30 -0400

    Re-styled login screen

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -38,8 +38,8 @@ class Home extends Component {
     const { inputVal } = this.state;
     const inputFormat = 'bg-gray-200 focus:bg-white border-transparent focus:border-blue-400 text-gray-900 appearance-none inline-block w-full text-gray-900 border rounded py-3 px-4 mb-3 focus:outline-none';
     return (
-      <div>
-        <form onSubmit={this.logIn}>
+      <div className='w-full max-w-xs m-auto'>
+{/*        <form onSubmit={this.logIn}>
           <div className='w-full md:w-1/2 px-3 mb-6 md:mb-0'>
             <input
               className={inputFormat}
@@ -50,13 +50,41 @@ class Home extends Component {
               onChange={e => this.handleChange(e, 'inputVal')}
             />
           </div>
-          {/* Upload image button */}
           <div className='w-full md:w-1/2 px-3'>
             <label className='text-center block w-full bg-blue-500 hover:bg-blue-700 text-white font-bold border-red-500 rounded py-3 px-4 mb-3 leading-tight'>
               <button type='submit'>Login</button>
             </label>
           </div>
-        </form>
+        </form>*/}
+
+        {/*<div className='w-full max-w-xs'>*/}
+          <form className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 bg-gray-200' onSubmit={this.logIn}>
+            <div className='mb-4'>
+              <label className='block text-gray-700 text-sm font-bold mb-2' for='username'>
+                Username
+              </label>
+              <input onChange={e => this.handleChange(e, 'inputVal')} className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' id='username' type='text' placeholder='Username'/>
+            </div>
+            {/*<div className='mb-6'>
+              <label className='block text-gray-700 text-sm font-bold mb-2' for='password'>
+                Password
+              </label>
+              <input className='shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline' id='password' type='password' placeholder='******************'/>
+              <p className='text-red-500 text-xs italic'>Please choose a password.</p>
+            </div>*/}
+            <div className='flex items-center justify-between'>
+              <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' type='submit'>
+                Sign In
+              </button>
+              {/*<a className='inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800' href='#'>
+                Forgot Password?
+              </a>*/}
+            </div>
+          </form>
+          <p className='text-center text-gray-500 text-xs'>
+            &copy;2019 RecipeJoiner All rights reserved.
+          </p>
+          {/*</div>*/}
       </div>
     );
   }
