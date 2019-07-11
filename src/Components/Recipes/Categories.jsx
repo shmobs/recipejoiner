484833fe29004c3b58461f4ab99181a694ff1552
Perry Raskin <perryraskin@gmail.com>
2019-07-10 22:02:21 -0400

    Added includeDelete boolean for Category component

--- src/Components/Recipes/Categories.jsx
+++ src/Components/Recipes/Categories.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 import Category from './Category';
 
 function Categories(props) {
-  const { deleteCategory, categories } = props;
-  const cats = categories.map((c, i) => (<Category key={c} category={c} handleDelete={() => deleteCategory(i)} />));
+  const { includeDelete, deleteCategory, categories } = props;
+  let cats;
+  console.log(includeDelete);
+  if (includeDelete === 'true') {
+    cats = categories.map((c, i) => (<Category key={c} category={c} includeDelete={includeDelete} handleDelete={() => deleteCategory(i)} />));
+  } else {
+    cats = categories.map((c, i) => (<Category key={c} category={c} includeDelete={includeDelete} />));
+  }
+
   return (
     <div className='flex flex-wrap mb-2'>
       { cats }
