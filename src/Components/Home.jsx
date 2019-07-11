ec2d1854495b600b2950d9f0b706a2bebfedf586
Perry Raskin <perryraskin@gmail.com>
2019-07-10 23:07:29 -0400

    Styled login on Home component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -36,17 +36,28 @@ class Home extends Component {
 
   render() {
     const { inputVal } = this.state;
+    const labelFormat = 'block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2';
+    // const inputFormat = 'border-transparent block w-full bg-gray-200 text-gray-700 rounded py-3 px-4 mb-3 leading-tight focus:border-blue-400 focus:outline-none focus:bg-white';
+    const inputFormat = 'bg-gray-200 focus:bg-white border-transparent focus:border-blue-400 text-gray-900 appearance-none inline-block w-full text-gray-900 border rounded py-3 px-4 mb-3 focus:outline-none';
     return (
       <div>
         <form onSubmit={this.logIn}>
-          <input
-            type='text'
-            name='inputVal'
-            placeholder='Username (email)'
-            value={inputVal}
-            onChange={e => this.handleChange(e, 'inputVal')}
-          />
-          <button type='submit'>Login</button>
+          <div className='w-full md:w-1/2 px-3 mb-6 md:mb-0'>
+            <input
+              className={inputFormat}
+              type='text'
+              name='inputVal'
+              placeholder='Username (email)'
+              value={inputVal}
+              onChange={e => this.handleChange(e, 'inputVal')}
+            />
+          </div>
+          {/* Upload image button */}
+          <div className='w-full md:w-1/2 px-3'>
+            <label className='text-center block w-full bg-blue-500 hover:bg-blue-700 text-white font-bold border-red-500 rounded py-3 px-4 mb-3 leading-tight'>
+              <button type='submit'>Login</button>
+            </label>
+          </div>
         </form>
       </div>
     );
